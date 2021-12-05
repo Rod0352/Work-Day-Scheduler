9c2dadf211f89b195d6b725eafa847b7ac08e972
Rod0352 <92071426+Rod0352@users.noreply.github.com>
2021-12-05 16:42:35 -0500

    fixed issue with the time tracker function and corrected spelling in syntax

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,38 +6,38 @@ $("#currentDay").html(todaysDate);
 $(document).ready(function () {
     $(".saveBtn").on("click", function () {
 
-        var textInput = $(this).siblings(".discription").val();
+        var textInput = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
         // save in local storage
-        localStorage.setItem(textInput, time);
-    })
-});
-
-// function to track time
-
-function trackTime() {
-    var nowTime = moment().hour();
-    $(".time-block").each(function () {
-        var hourBlock = parseInt($(this).attr("id").split("hour")[1]);
-        console.log(hourBlock, nowTime);
-
-        if (hourBlock < nowTime) {
-            $(this).addClass("past");
-            $(this).addClass("future");
-            $(this).removeClass("present");
-        }
-        else if (hourBlock === nowTime) {
-            $(this).removeClass("past");
-            $(this).addClass("present");
-            $(this).removeClass("future");
-        }
-        else {
-            $(this).removeClass("present");
-            $(this).removeClass("past");
-            $(this).addClass("future");
-        }
+        localStorage.setItem(time, textInput);
     })
 
+
+    // function to track time
+
+    function trackTime() {
+        var nowTime = moment().hour();
+        $(".time-block").each(function () {
+            var hourBlock = parseInt($(this).attr("id").split("hr")[1]);
+
+            if (hourBlock < nowTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (hourBlock === nowTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    };
     $("#hr9 .description").val(localStorage.getItem("hr9"));
     $("#hr10 .description").val(localStorage.getItem("hr10"));
     $("#hr11 .description").val(localStorage.getItem("hr11"));
@@ -48,5 +48,5 @@ function trackTime() {
     $("#hr16 .description").val(localStorage.getItem("hr16"));
     $("#hr17 .description").val(localStorage.getItem("hr17"));
 
-}
-trackTime();
+    trackTime();
+})
